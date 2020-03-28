f2a7c2f0c3384462393ff3fcb0d9255194d5a942
Ибрагим Аушев <aushevibra@yandex.ru>
2020-03-28 20:43:13 +0300

    fix

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', '/login', 'airlock/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '/login',],
 
     'allowed_methods' => ['*'],
 
